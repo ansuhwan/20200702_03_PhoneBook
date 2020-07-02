a394007a8c24f710771c48343a136bdf21eaa0f1
안수환 <dkstn1230@naver.com>
2020-07-02 12:37:54 +0900

    메뉴 띄우기 선택값 입력
    무한으로 선택 계속 입력 받는 구조

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,11 +1,36 @@
 package codes;
 
+import java.util.Scanner;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class MainDrive {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Git 와 연동해사 전화번호부 만들 예정
-		
+		printMenu();
+	}
+
+	public static void printMenu() {
+
+		Scanner myScan = new Scanner(System.in);
+
+		while (true) {
+			
+//			어떤 메뉴가 있는지 표기
+			System.out.println("*******전화번호부*******");
+			System.out.println("1) 전화번호 추가  등록");
+			System.out.println("2) 전체 전화 번호 목록 조회");
+			System.out.println("0) 프로그램 종료");
+			System.out.println("=====================");
+			
+//			실제 메뉴 입력 받기
+			System.out.println("메뉴 선택 :");
+			int inputMenu = myScan.nextInt();
+
+		}
+
 	}
 
 }
